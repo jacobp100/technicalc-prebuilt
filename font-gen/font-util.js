a1fce5cee36ef017c6dd9031354db34c7043723b
Jacob Parker <jacobparker1992@gmail.com>
2020-10-11 22:27:00 +0100

    .

--- font-gen/font-util.js
+++ font-gen/font-util.js
@@ -1,15 +1,3 @@
-module.exports.translateX = (char, tx) => {
-  const data = char.slice();
-  const p = data
-    .pop()
-    .p.replace(
-      /(-?\d+)\s+(-?\d+)/g,
-      (_, x, y) => `${(+x + tx).toFixed(0)} ${y}`
-    );
-  data.push({ p });
-  return data;
-};
-
 module.exports.skewX = (char, deg) => {
   const c = Math.atan((deg * 2 * Math.PI) / 360);
   const data = char.slice();
